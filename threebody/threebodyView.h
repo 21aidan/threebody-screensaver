4e731ca367a1503eceaf6504da2f1778fb7279bb
Aidann21 <104369485+21aidan@users.noreply.github.com>
2025-01-15 20:00:46 +0000

    Initial Commit

---
+++ threebody/threebodyView.h
@@ -0,0 +1,12 @@
+//
+//  threebodyView.h
+//  threebody
+//
+//  Created by Aidan Nash on 15/01/2025.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface threebodyView : ScreenSaverView
+
+@end
---
+++ threebody/threebodyView.m
@@ -0,0 +1,51 @@
+//
+//  threebodyView.m
+//  threebody
+//
+//  Created by Aidan Nash on 15/01/2025.
+//
+
+#import "threebodyView.h"
+
+@implementation threebodyView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
